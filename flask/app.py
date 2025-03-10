f79626083ad4166d4d2c3612604361052a7427f7
Life Giver <lifefotsing@gmail.com>
2025-03-10 07:26:50 +0100

    addition_of_toggle_bar_for_mobile

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route('/landing_page')
 def style():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Welcome to Scroll Saga")
+
+
+@app.route("/about")
+def about():
+    return render_template("default_page.html", page_title="About Us")
 
 
 if __name__ == "__main__":
