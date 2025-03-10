d20164e5fea8e3ed04def09421963f219419540a
Life Giver <lifefotsing@gmail.com>
2025-03-10 10:04:18 +0100

    creation_of_routes

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html", page_title="About Us")
+
+
+@app.route("/register")
+def register():
+    return render_template("signUp.html", page_title="About Us")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("user_details.html", page_title="About Us")
+
+
+@app.route("/admin_panel")
+def panel():
+    return render_template("admin_pannel.html", page_title="About Us")
+
+
 @app.route('/landing_page')
 def style():
     return render_template("index.html", page_title="Welcome to Scroll Saga")
